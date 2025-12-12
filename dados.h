a81c12bcec90e6de36333db3e14eff2bfa7d44c5
ramosestela91-dot <ramosestela91@gmail.com>
2025-12-12 09:51:26 -0300

    Add files via upload
    dados.cpp

---
+++ dados.h
@@ -0,0 +1,6 @@
+#pragma once
+void dibujarCuadrado(int posx , int posy);
+void dibujarDado(int numero, int posx , int posy);
+void dibujarSombra(int posx, int posy);
+void dibujarPuntos(int numero, int posx, int posy);
+
